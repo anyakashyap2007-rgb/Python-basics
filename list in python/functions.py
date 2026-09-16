69b08682780eadb9ed3541df9bc9a55d8aedf56f
Anya <anyakashyap2007@gmail.com>
2026-09-16 09:29:37 +0530

    CHANGE

--- list in python/functions.py
+++ list in python/functions.py
@@ -12,3 +12,20 @@ num.sort()
 print("sorted list:",num)
 num.sort(reverse=True)
 print(num)
+
+#count()
+numbers = [0,1,3,4,1,0,5,0,3,3,0]
+print(numbers.count(0))
+
+#user input 
+print(f"the list is:{numbers}")
+item_to_count= int(input("enter the numer to be counted from the list:"))
+c= numbers.count(item_to_count)
+print(f"occurrence of {item_to_count} is {c}")
+
+#Membership operation
+
+#in fuction ,not in 
+language =["C++","Python","English"]
+print("C++" in language)
+print("hindi" not in language)
---
+++ list in python/num_operators.py
@@ -0,0 +1,15 @@
+numbers =[10,4,5,5,7,1]
+
+#smallest number in the list 
+#min()
+print(min(numbers))
+print(f"smallest num:{min(numbers)}")
+
+#largest number in the list 
+#max()
+print(f"largest num:{max(numbers)}")
+
+
+#total of the numbers of the list 
+#sum 
+print(f"Total:{sum(numbers)}")
